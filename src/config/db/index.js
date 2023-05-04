c11401765818329539e9553fbe5123d9199c2c2f
minh1304 <vtm1304@gmail.com>
2023-05-04 10:57:39 +0700

    deloy cyclic

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 async function connect() {
     try {
-        await mongoose.connect('mongodb://localhost:27017/store_dev', {});
+        // await mongoose.connect('mongodb://localhost:27017/store_dev', {});
+        await mongoose.connect('mongodb+srv://tuanminhvo:a0858251139@cluster0.790t6qi.mongodb.net/test', {});
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!');
