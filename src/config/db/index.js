5ebe6dd4f394a800393fa94efe92f5a6d2edbaff
minh1304 <vtm1304@gmail.com>
2023-05-04 11:52:59 +0700

    update

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
+require('dotenv').config();
 async function connect() {
     try {
         // await mongoose.connect('mongodb://localhost:27017/store_dev', {});
-        await mongoose.connect(process.env.MONGO_URI);
+        // await mongoose.connect('mongodb+srv://vtm1304:IPMRPli2KcJIMCG8@cluster0.i5ziru3.mongodb.net/test', {});
+        await mongoose.connect(process.env.MONGODB_URI, {});
         console.log('Connect successfully!!!');
     } catch (error) {
         console.log('Connect failure!!!');
